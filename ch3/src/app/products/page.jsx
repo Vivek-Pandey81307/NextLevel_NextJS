24299d95bc3193aa9cdbd95fbcef29ed4780a5e6
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-11 22:50:32 +0530

    nested routing

---
+++ ch3/src/app/products/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <>
+    <h1>Products Page</h1>
+    <hr />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatem cum asperiores? Doloribus delectus modi, veniam suscipit ratione expedita numquam nulla velit ad.</p>
+    </>
+  )
+}
+
+export default Products
