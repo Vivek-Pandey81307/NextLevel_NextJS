f3f25ce4fae9bf451ea7c7388c8dee611f612762
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-12 22:12:46 +0530

    Parallel Routes or Conditional Route Render

---
+++ ch3/src/app/colorbox/layout.js
@@ -0,0 +1,13 @@
+export default function ColorBoxLayout({ children,left,right }) {
+    const isSideBar = true;
+    return (
+
+    <section>
+        {children}
+        {/* {left}
+        {right} */}
+        {isSideBar ? left :right}
+
+    </section>
+ )
+} 
