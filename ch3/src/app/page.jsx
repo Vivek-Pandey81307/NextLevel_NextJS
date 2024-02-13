dfdb9c112b67deadbe82c3ff8e2d0cc1602c4bbd
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-13 23:25:45 +0530

    using CSS modules

--- ch3/src/app/page.jsx
+++ ch3/src/app/page.jsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import LearnLink from "./components/LearnLink";
-import LearnUseRouter from "./components/LearnUseRouter";
-import LearnServerComponent from "./components/LearnServerComponent";
-import LearnClientComponent from "./components/LearnClientComponent";
-import LearnGlobalCSS from "./components/LearnGlobalCSS";
+import LearnLink from "../components/LearnLink";
+import LearnUseRouter from "../components/LearnUseRouter";
+import LearnServerComponent from "../components/LearnServerComponent";
+import LearnClientComponent from "../components/LearnClientComponent";
+import LearnGlobalCSS from "../components/LearnGlobalCSS";
+import LearnCSSModule from "@/components/LearnCSSModule";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     {/* <LearnLink /> */}
-     {/* <LearnUseRouter/> */}
-     {/* <LearnServerComponent/> */}
-    <LearnGlobalCSS/>
+      <LearnLink />
+      <LearnUseRouter />
+      <LearnServerComponent />
+      <LearnGlobalCSS />
+      <LearnCSSModule />
     </main>
   );
 }
