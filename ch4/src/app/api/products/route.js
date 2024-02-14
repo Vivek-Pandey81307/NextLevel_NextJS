c9f50e42d3057774148a512ee54b23bcaa7aeb1e
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-15 03:25:42 +0530

    Post Request

--- ch4/src/app/api/products/route.js
+++ ch4/src/app/api/products/route.js
@@ -6,7 +6,17 @@ export async function GET(req){
     //  const {searchParams}= new URL(req.url);
     // const searchParams = req.nextUrl.searchParams 
     // console.log(searchParams);
-    const cook1 =req.cookies
-    console.log("Cookie1:",cook1);
+    // const cook1 =req.cookies
+    // console.log("Cookie1:",cook1);
+    // const cook2 =cookies();
+    // console.log("Cookie2:",cook2)
     return NextResponse.json({"msg":"Ello ji Nextjs api"})
+}
+export async function POST(req){
+    // console.log(req);
+    // const res = await req.json();
+    // console.log("ResJSON:",res)
+    const formData = await req.formData();
+    console.log("Form Data: ",formData)
+    return NextResponse.json({"msg":"Post request accomplished"},{status:201})
 }
