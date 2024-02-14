3b9d71e00aa57112e55c5f2d5a46f9458c06cc13
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-15 02:00:17 +0530

    Headers(req.headers)

---
+++ ch4/src/app/api/products/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server"
+export async function GET(req){
+    // console.log(req)
+    const requestHeaders = new Headers(req.headers);
+    console.log(requestHeaders)
+    return NextResponse.json({"msg":"Ello ji Nextjs api"})
+}
