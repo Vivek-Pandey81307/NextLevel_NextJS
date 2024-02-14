21670da1051f93bed5834ff9c9436715eea54c6e
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-15 02:21:41 +0530

    Url query params

--- ch4/src/app/api/products/route.js
+++ ch4/src/app/api/products/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server"
 export async function GET(req){
     // console.log(req)
-    const requestHeaders = new Headers(req.headers);
-    console.log(requestHeaders)
+    // const requestHeaders = new Headers(req.headers);
+    // console.log(requestHeaders)
+     const {searchParams}= new URL(req.url);
+     console.log(searchParams);
     return NextResponse.json({"msg":"Ello ji Nextjs api"})
 }
