2998baada86c6aa7fd524d54671bfa6040913cdb
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-15 02:25:09 +0530

    nextUrl

--- ch4/src/app/api/products/route.js
+++ ch4/src/app/api/products/route.js
@@ -3,7 +3,8 @@ export async function GET(req){
     // console.log(req)
     // const requestHeaders = new Headers(req.headers);
     // console.log(requestHeaders)
-     const {searchParams}= new URL(req.url);
-     console.log(searchParams);
+    //  const {searchParams}= new URL(req.url);
+    const searchParams = req.nextUrl.searchParams 
+    console.log(searchParams);
     return NextResponse.json({"msg":"Ello ji Nextjs api"})
 }
