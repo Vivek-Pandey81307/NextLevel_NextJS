40561d056baa2305444cc1a97b68a4c997e0dfff
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-15 09:19:13 +0530

    Third Party Req from Server

--- ch4/src/app/page.js
+++ ch4/src/app/page.js
@@ -1,9 +1,27 @@
-import Image from "next/image";
-
-export default function Home() {
+const getData = async()=>{
+  const res = await fetch("http://localhost:3000/api/blog/posts",{
+    headers:{
+      'Content-Type':'application/json', 
+    }
+  })
+  const posts = await res.json();
+  return posts;
+}
+export default async function Home() {
+  const allposts = await getData()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello Next</h1>
+     {
+      allposts.data.map((post,i)=>
+        <div key={i}>
+          
+          <h1>{post.tile}</h1>
+          <br/>
+          <p>{post.body}</p>
+          <hr /><hr/><hr/>
+        </div>
+      )
+     }
     </main>
   );
 }
