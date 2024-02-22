65b0b9e2ee4896693be78b937ece43f1c907f2bd
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-22 08:25:13 +0530

    mongodb error solved-{DATABASE_URL}

--- ch5/src/app/api/posts/route.js
+++ ch5/src/app/api/posts/route.js
@@ -4,22 +4,24 @@ import  PostModel from '@/models/Post'
 export async function GET(req){
     try{
         await connectDB()
-        const result = await PostModel.findOne()
-        return NextResponse.json({"result":result},{"msg":"success"},{status:200})
+        const result = await PostModel.find()
+        return NextResponse.json(
+            {"result":result},
+            {"msg":"success"},{status:200})
     }catch(error){
         console.log(error)
         return NextResponse.json({"msg":"something went wrong"},{status:400})
     }
 }
 
-export async function POST(req){
-    const body = await req.json()
-    try{
-        await connectDB()
-        const result = await PostModel.create(body)
-        return NextResponse.json({"result":result},{"msg":"success"},{status:201})
-    }catch(error){
-        console.log(error)
-        return NextResponse.json({"msg":"something went wrong"},{status:400})
-    }
-}
+// export async function POST(req){
+//     const body = await req.json()
+//     try{
+//         await connectDB()
+//         const result = await PostModel.create(body)
+//         return NextResponse.json({"result":result},{"msg":"success"},{status:201})
+//     }catch(error){
+//         console.log(error)
+//         return NextResponse.json({"msg":"something went wrong"},{status:400})
+//     }
+// }
--- ch5/src/lib/connectDB.js
+++ ch5/src/lib/connectDB.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 export default async ()=>{
     try{
-        
         await mongoose.connect (process.env.DATABASE_URL)
         console.log('Connected Successfully ...');
     }catch(error){
