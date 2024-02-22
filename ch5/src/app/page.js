c87732e0b885207847fe505fa4b2b1daa9bf5509
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-02-22 12:57:42 +0530

    GetData2.jsx-Server Action

--- ch5/src/app/page.js
+++ ch5/src/app/page.js
@@ -1,12 +1,13 @@
 import GetData1 from "@/components/GetData1";
+import GetData2 from "@/components/GetData2";
 import PostData from "@/components/PostData";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <PostData />
+    {/* <PostData />
     <GetData1 />
-    
- 
+     */}
+    <GetData2/>
     </main>
      
   );
